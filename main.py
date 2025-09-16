8e33ae42290ad2cbe57160b7ce1a05976e04af61
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2025-09-16 10:34:41 +0200

    Aktualisiere pxt.json, main.ts, pxt.json, _history, main.py

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-Hand = 0
+def on_button_a():
+    global Control
+    Control = 0
+    basic.show_string("H")
+    while True:
+        if Control == 1:
+            break
+        basic.show_icon(IconNames.SAD)
+        basic.pause(100)
+        if Control == 1:
+            break
+        basic.show_icon(IconNames.HAPPY)
+input.on_button_event(Button.A, input.button_event_click(), on_button_a)
+
+def on_button_b():
+    global Control
+    Control = 1
+input.on_button_event(Button.B, input.button_event_click(), on_button_b)
 
 def on_gesture_shake():
     global Hand
     Hand = randint(1, 3)
     if Hand == 1:
-        basic.show_icon(IconNames.HEART)
+        basic.show_icon(IconNames.SCISSORS)
     if Hand == 2:
-        basic.show_icon(IconNames.NO)
+        basic.show_icon(IconNames.CHESSBOARD)
     if Hand == 3:
-        basic.show_icon(IconNames.TSHIRT)
+        basic.show_leds("""
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            """)
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+Hand = 0
+Control = 0
+Control = 0
--- main.ts
+++ main.ts
@@ -1,29 +1,36 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+input.onButtonEvent(Button.A, input.buttonEventClick(), function on_button_a() {
+    
     Control = 0
     basic.showString("H")
     while (true) {
         if (Control == 1) {
-            break;
+            break
         }
+        
         basic.showIcon(IconNames.Sad)
         basic.pause(100)
         if (Control == 1) {
-            break;
+            break
         }
+        
         basic.showIcon(IconNames.Happy)
     }
 })
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+input.onButtonEvent(Button.B, input.buttonEventClick(), function on_button_b() {
+    
     Control = 1
 })
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
     Hand = randint(1, 3)
     if (Hand == 1) {
         basic.showIcon(IconNames.Scissors)
     }
+    
     if (Hand == 2) {
         basic.showIcon(IconNames.Chessboard)
     }
+    
     if (Hand == 3) {
         basic.showLeds(`
             # # # # #
@@ -33,6 +40,7 @@ input.onGesture(Gesture.Shake, function () {
             # # # # #
             `)
     }
+    
 })
 let Hand = 0
 let Control = 0
