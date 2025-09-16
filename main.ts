30e4934f7a279a814269e6c37d6d3576ce77e73e
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2025-09-16 10:46:55 +0200

    Aktualisiere pxt.json, main.blocks, main.ts, pxt.json, _history

--- main.ts
+++ main.ts
@@ -1,36 +1,35 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function on_button_a() {
-    
-    Control = 0
-    basic.showString("H")
-    while (true) {
-        if (Control == 1) {
-            break
-        }
-        
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showString("Hallo, wie geht es euch?")
+    for (let index = 0; index < 4; index++) {
         basic.showIcon(IconNames.Sad)
         basic.pause(100)
-        if (Control == 1) {
-            break
-        }
-        
         basic.showIcon(IconNames.Happy)
     }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    for (let index = 0; index < 4; index++) {
+        basic.setLedColors(0x0000ff, 0x0000ff, 0x0000ff)
+        basic.pause(1000)
+        basic.setLedColors(0xff0000, 0xff0000, 0xff0000)
+        basic.pause(1000)
+    }
 })
-input.onButtonEvent(Button.B, input.buttonEventClick(), function on_button_b() {
-    
-    Control = 1
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+	
 })
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
-    
+input.onGesture(Gesture.Shake, function () {
     Hand = randint(1, 3)
     if (Hand == 1) {
         basic.showIcon(IconNames.Scissors)
     }
-    
     if (Hand == 2) {
         basic.showIcon(IconNames.Chessboard)
     }
-    
     if (Hand == 3) {
         basic.showLeds(`
             # # # # #
@@ -40,8 +39,9 @@ input.onGesture(Gesture.Shake, function on_gesture_shake() {
             # # # # #
             `)
     }
-    
 })
 let Hand = 0
-let Control = 0
-Control = 0
+radio.setGroup(1)
+basic.forever(function () {
+    radio.sendString("\"Hey, bist Du da?\"")
+})
